e53f3ec8dbc1cc3e32d57c08604b2b1d51661ca4
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-12 19:51:00 +0800

    fix: ref

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -5,7 +5,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
-    '@antfu/eslint-config',
+    '@antfu/eslint-config-basic',
   ],
   rules: {
     // TS
