9847ac276b74f8b6471dcaff467d0f0503783437
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-15 01:58:43 +0800

    chore: fix

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -186,6 +186,7 @@ module.exports = {
     // Use new when throwing error
     'unicorn/throw-new-error': 'error',
 
+    'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
     'eslint-comments/disable-enable-pair': 'off',
   },
 }
