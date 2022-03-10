2e962a278571113853435221e8c3d43bd9fa1421
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-11 05:55:22 +0800

    feat: improve `no-unused-vars` rule

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -25,7 +25,7 @@ module.exports = {
     'indent': 'off',
     '@typescript-eslint/indent': ['error', 2],
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': 'error',
     'no-use-before-define': 'off',
