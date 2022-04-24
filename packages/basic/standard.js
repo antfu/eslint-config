17670e8e3876ffc080b6635c63a35746f5fafd86
liyujun <90846188+liyujun-dev@users.noreply.github.com>
2022-04-25 01:02:25 +0800

    fix: space-before-function-paren allows space in anonymous and asyncArrow function. (#84)

--- packages/basic/standard.js
+++ packages/basic/standard.js
@@ -202,7 +202,14 @@ module.exports = {
     'semi': ['error', 'never'],
     'semi-spacing': ['error', { before: false, after: true }],
     'space-before-blocks': ['error', 'always'],
-    'space-before-function-paren': ['error', 'always'],
+    'space-before-function-paren': [
+      'error',
+      {
+        'anonymous': 'always',
+        'named': 'never',
+        'asyncArrow': 'always'
+      }
+    ],
     'space-in-parens': ['error', 'never'],
     'space-infix-ops': 'error',
     'space-unary-ops': ['error', { words: true, nonwords: false }],
