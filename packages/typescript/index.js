7484ae958f3b7cf39bed5b37d0529c9182bf205e
zz <2418184580@qq.com>
2022-05-02 11:36:37 +0800

    fix: deprecated rules
    https://github.com/typescript-eslint/typescript-eslint/blob/88ed9ec9d6b971a9533565920fdcd6890ea941e9/packages/eslint-plugin/src/rules/no-parameter-properties.ts#L23
    
    https://typescript-eslint.io/rules/camelcase

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -114,11 +114,11 @@ module.exports = {
     // '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
 
     // off
-    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/naming-convention': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/parameter-properties': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/no-empty-function': 'off',
