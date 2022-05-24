a9d330cac36a92ab9e740f68a758df11e33197e3
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-25 05:31:00 +0800

    fix: disable `vue/prefer-import-from-vue`

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -23,6 +23,7 @@ module.exports = {
     'vue/require-prop-types': 'off',
     'vue/require-default-prop': 'off',
     'vue/multi-word-component-names': 'off',
+    'vue/prefer-import-from-vue': 'off',
 
     // reactivity transform
     'vue/no-setup-props-destructure': 'off',
