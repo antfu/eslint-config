86623530d3792824e59beb1d01beb2bbe7c064c3
曾明健 <hellozmj@qq.com>
2022-09-02 18:13:24 +0800

    feat: remove `@typescript-eslint/consistent-type-imports`
    remove `@typescript-eslint/consistent-type-imports` to improve compatibility for Angular and Nest

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -19,7 +19,6 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
     '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
     '@typescript-eslint/type-annotation-spacing': ['error', {}],
-    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
     '@typescript-eslint/prefer-ts-expect-error': 'error',
 
