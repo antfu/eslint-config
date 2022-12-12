b93f48f4295b8935b20088d8370a1b35071f28d7
Kirk Lin <linkirk@163.com>
2022-12-13 01:01:22 +0800

    feat(ts): add TypeScript Aware Rules from @typescript-eslint/recommen… (#135)

--- packages/eslint-plugin-antfu/src/rules/prefer-inline-type-import.ts
+++ packages/eslint-plugin-antfu/src/rules/prefer-inline-type-import.ts
@@ -1,6 +1,7 @@
 // Ported from https://github.com/gajus/eslint-plugin-canonical/blob/master/src/rules/preferInlineTypeImport.js
 // by Gajus Kuizinas https://github.com/gajus
-
+import type { TSESTree } from '@typescript-eslint/utils'
+import type { RuleFixer, SourceCode } from '@typescript-eslint/utils/dist/ts-eslint'
 import { createEslintRule } from '../utils'
 
 export const RULE_NAME = 'prefer-inline-type-import'
@@ -47,7 +48,7 @@ export default createEslintRule<Options, MessageIds>({
   },
 })
 
-function *removeTypeSpecifier(fixer, sourceCode, node) {
+function * removeTypeSpecifier(fixer: RuleFixer, sourceCode: Readonly<SourceCode>, node: TSESTree.ImportDeclaration) {
   const importKeyword = sourceCode.getFirstToken(node)
 
   const typeIdentifier = sourceCode.getTokenAfter(importKeyword)
