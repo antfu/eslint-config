cdb02c8fa14381acfb17c426496c2854a8d2e5f7
Anthony Fu <anthonyfu117@hotmail.com>
2023-05-21 14:19:11 +0200

    fix: turn off cjs-check in markdown

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -202,6 +202,8 @@ module.exports = {
         'no-undef': 'off',
         'no-unused-expressions': 'off',
         'no-unused-vars': 'off',
+        'antfu/no-cjs-exports': 'off',
+        'antfu/no-ts-export-equal': 'off',
       },
     },
   ],
