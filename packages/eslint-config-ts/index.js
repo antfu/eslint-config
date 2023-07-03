d4a9ebcc179f54d9abfe284bff9548ed57fbdaf0
曾明健 <hellozmj@qq.com>
2023-07-04 03:19:59 +0800

    feat(ts): enable `@typescript-eslint/ban-types` (#210)

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -173,7 +173,6 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
     // handled by unused-imports/no-unused-imports
     '@typescript-eslint/no-unused-vars': 'off',
