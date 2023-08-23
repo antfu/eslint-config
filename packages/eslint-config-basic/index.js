10d38567fa614ffe7e7d51cd520ad2e8a1e5a3f2
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-23 12:58:12 +0200

    feat: sort `resolutions` in package.json

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -139,6 +139,14 @@ module.exports = {
             order: { type: 'asc' },
           },
           {
+            pathPattern: '^resolutions$',
+            order: { type: 'asc' },
+          },
+          {
+            pathPattern: '^pnpm.overrides$',
+            order: { type: 'asc' },
+          },
+          {
             pathPattern: '^exports.*$',
             order: [
               'types',
