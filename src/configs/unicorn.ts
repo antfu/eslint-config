87282257cf52b0bab6141aca9150640713da8a3c
Donny Wang <53386850+coderwyd@users.noreply.github.com>
2023-10-07 14:06:45 +0800

    fix(unicorn): rename `prefer-text-content` -> `prefer-dom-node-text-c… (#271)

--- src/configs/unicorn.ts
+++ src/configs/unicorn.ts
@@ -21,6 +21,8 @@ export function unicorn(): FlatESLintConfigItem[] {
         'unicorn/no-new-buffer': 'error',
         // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
         'unicorn/number-literal-case': 'error',
+        // textContent instead of innerText
+        'unicorn/prefer-dom-node-text-content': 'error',
         // includes over indexOf when checking for existence
         'unicorn/prefer-includes': 'error',
         // Prefer using the node: protocol
@@ -29,8 +31,6 @@ export function unicorn(): FlatESLintConfigItem[] {
         'unicorn/prefer-number-properties': 'error',
         // String methods startsWith/endsWith instead of more complicated stuff
         'unicorn/prefer-string-starts-ends-with': 'error',
-        // textContent instead of innerText
-        'unicorn/prefer-text-content': 'error',
         // Enforce throwing type error when throwing error while checking typeof
         'unicorn/prefer-type-error': 'error',
         // Use new when throwing error
