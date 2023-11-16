b9f454fe502ebc093e3db745fa07c941d5c7e401
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-16 10:19:22 +0100

    chore: update snap

--- fixtures/output/no-style/jsx.jsx
+++ fixtures/output/no-style/jsx.jsx
@@ -9,17 +9,13 @@ export function HelloWorld({
   .replace(/\.\d+/ig, "")
 
   return <div className='HelloWorld' title={`You are visitor number ${ num }`} onMouseOver={onMouseOver}>
-
     <strong>{ greeting.slice( 0, 1 ).toUpperCase() + greeting.slice(1).toLowerCase() }</strong>
     {greeting.endsWith(",") 
     ? " " : <span style={{color: '\grey'}}>", "</span> }
     <em>
   { greeted }
   </em>
-    { (silent)
-      ? "."
-      : "!"}
-
+    { (silent)? ".": "!"}
     </div>;
 
 }
--- fixtures/output/tab-double-quotes/jsx.jsx
+++ fixtures/output/tab-double-quotes/jsx.jsx
@@ -13,7 +13,6 @@ export function HelloWorld({
 
 	return (
 		<div className="HelloWorld" title={`You are visitor number ${num}`} onMouseOver={onMouseOver}>
-
 			<strong>{ greeting.slice(0, 1).toUpperCase() + greeting.slice(1).toLowerCase() }</strong>
 			{greeting.endsWith(",")
     	? " "
@@ -21,10 +20,7 @@ export function HelloWorld({
 			<em>
 				{ greeted }
 			</em>
-			{ (silent)
-    	? "."
-    	: "!"}
-
+			{ (silent) ? "." : "!"}
 		</div>
 	)
 }
