4bb5c669c20f9f32abb4fec08330c56a534293c0
Cuong Nguyen <dr.yami.moe@gmail.com>
2023-11-19 15:30:13 +0200

    moved plugins react to optional peer, inlined react config

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -38,11 +38,33 @@ export async function react(
       },
       name: 'antfu:react:rules',
       rules: {
-        // recommended rules react
-        ...pluginReact.configs.recommended.rules as any,
-        ...pluginReactHooks.configs.recommended.rules as any,
+        // recommended rules react-hooks
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/rules-of-hooks': 'error',
 
+        // recommended rules react
+        'react/display-name': 'error',
+        'react/jsx-key': 'error',
+        'react/jsx-no-comment-textnodes': 'error',
+        'react/jsx-no-duplicate-props': 'error',
+        'react/jsx-no-target-blank': 'error',
+        'react/jsx-no-undef': 'error',
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/no-children-prop': 'error',
+        'react/no-danger-with-children': 'error',
+        'react/no-deprecated': 'error',
+        'react/no-direct-mutation-state': 'error',
+        'react/no-find-dom-node': 'error',
+        'react/no-is-mounted': 'error',
+        'react/no-render-return-value': 'error',
+        'react/no-string-refs': 'error',
+        'react/no-unescaped-entities': 'error',
+        'react/no-unknown-property': 'error',
+        'react/no-unsafe': 'off',
+        'react/prop-types': 'error',
         'react/react-in-jsx-scope': 'off',
+        'react/require-render-return': 'error',
 
         ...typescript
           ? {
