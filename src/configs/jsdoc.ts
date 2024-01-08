eff6c03cbc78f44698c8d5bfc573621f354c1c97
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-08 11:11:30 +0100

    chore: update deps

--- src/configs/jsdoc.ts
+++ src/configs/jsdoc.ts
@@ -10,7 +10,6 @@ export async function jsdoc(options: OptionsStylistic = {}): Promise<FlatConfigI
     {
       name: 'antfu:jsdoc',
       plugins: {
-        // @ts-expect-error missing types
         jsdoc: await interopDefault(import('eslint-plugin-jsdoc')),
       },
       rules: {
