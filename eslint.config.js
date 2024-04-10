22635388664aad2c91ef06fa1e4297f7e1ace9b2
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-10 03:36:05 +0200

    chore: update dev script

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,6 @@
 // @ts-check
-import styleMigrate from '@stylistic/eslint-plugin-migrate'
-import JITI from 'jiti'
+import { bundleRequire } from 'bundle-require'
 
-const jiti = JITI(import.meta.url)
-/**
- * @type {import('./src').default}
- */
-const antfu = jiti('./src').default
-
-export default antfu(
-  {
-    vue: true,
-    react: true,
-    solid: true,
-    svelte: true,
-    astro: true,
-    typescript: true,
-    formatters: true,
-  },
-  {
-    ignores: [
-      'fixtures',
-      '_fixtures',
-    ],
-  },
-  {
-    files: ['src/**/*.ts'],
-    rules: {
-      'perfectionist/sort-objects': 'error',
-    },
-  },
-  {
-    files: ['src/configs/*.ts'],
-    plugins: {
-      'style-migrate': styleMigrate,
-    },
-    rules: {
-      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
-    },
-  },
-)
+export default bundleRequire({
+  filepath: './eslint.config.ts',
+}).then(r => r.mod.default)
---
+++ eslint.config.ts
@@ -0,0 +1,36 @@
+// @ts-expect-error missing types
+import styleMigrate from '@stylistic/eslint-plugin-migrate'
+import { antfu } from './src'
+
+export default antfu(
+  {
+    vue: true,
+    react: true,
+    solid: true,
+    svelte: true,
+    astro: true,
+    typescript: true,
+    formatters: true,
+  },
+  {
+    ignores: [
+      'fixtures',
+      '_fixtures',
+    ],
+  },
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'perfectionist/sort-objects': 'error',
+    },
+  },
+  {
+    files: ['src/configs/*.ts'],
+    plugins: {
+      'style-migrate': styleMigrate,
+    },
+    rules: {
+      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
+    },
+  },
+)
