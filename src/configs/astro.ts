49a9d0c70e5effd4378324cb7b6d9e1204d933e4
Martín Acosta <tinchoz49@gmail.com>
2024-06-02 10:15:46 -0300

    feat: update astro plugin to v1.0 (#492)
    Co-authored-by: Anthony Fu <github@antfu.me>

--- src/configs/astro.ts
+++ src/configs/astro.ts
@@ -31,16 +31,28 @@ export async function astro(
     {
       files,
       languageOptions: {
+        globals: pluginAstro.environments.astro.globals,
         parser: parserAstro,
         parserOptions: {
           extraFileExtensions: ['.astro'],
-          parser: parserTs as any,
+          parser: parserTs,
         },
+        sourceType: 'module',
       },
       name: 'antfu/astro/rules',
+      processor: 'astro/client-side-ts',
       rules: {
+        // use recommended rules
+        'astro/missing-client-only-directive-value': 'error',
+        'astro/no-conflict-set-directives': 'error',
+        'astro/no-deprecated-astro-canonicalurl': 'error',
+        'astro/no-deprecated-astro-fetchcontent': 'error',
+        'astro/no-deprecated-astro-resolve': 'error',
+        'astro/no-deprecated-getentrybyslug': 'error',
         'astro/no-set-html-directive': 'off',
+        'astro/no-unused-define-vars-in-style': 'error',
         'astro/semi': 'off',
+        'astro/valid-compile': 'error',
 
         ...stylistic
           ? {
