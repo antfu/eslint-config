af2b60b1541cc8dc08e2f9e4c58ac920bb9a3a3a
Nir Tamir <nirtamir2@gmail.com>
2024-06-10 17:12:27 +0300

    fix storybook

--- src/configs/storybook.ts
+++ src/configs/storybook.ts
@@ -7,20 +7,17 @@ export async function storybook(): Promise<Array<TypedFlatConfigItem>> {
   await ensurePackages(["eslint-plugin-storybook"]);
 
   return [
-    {
-      name: "nirtamir2/storybook",
-      ...fixupConfigRules(
-        compat.config({
-          extends: [
-            "plugin:storybook/recommended",
-            "plugin:storybook/csf-strict",
-            "plugin:storybook/addon-interactions",
-          ],
-          // .eslintignore is not supported with flat config, make sure to ignore also other build and test folders
-          ignorePatterns: ["!.storybook", "storybook-static"],
-        }),
-      ),
-    },
+    ...fixupConfigRules(
+      compat.config({
+        extends: [
+          "plugin:storybook/recommended",
+          "plugin:storybook/csf-strict",
+          "plugin:storybook/addon-interactions",
+        ],
+        // .eslintignore is not supported with flat config, make sure to ignore also other build and test folders
+        ignorePatterns: ["!.storybook", "storybook-static"],
+      }),
+    ),
     {
       name: "nirtamir2/storybook/i18n",
       files: ["**.stories.tsx"],
