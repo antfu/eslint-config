238bcfa3aad810f23e86eb7ab4491e4cd9855b27
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-19 00:29:55 +0100

    feat(typescript): add 'ts/return-await' rule
    This commit adds the 'ts/return-await' rule to the TypeScript
    configuration. This rule enforces that 'return await' is used in async
    functions.

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -48,6 +48,7 @@ export async function typescript(
     'ts/no-unsafe-return': 'error',
     'ts/restrict-plus-operands': 'error',
     'ts/restrict-template-expressions': 'error',
+    'ts/return-await': 'error',
     'ts/strict-boolean-expressions': 'error',
     'ts/unbound-method': 'error',
   }
