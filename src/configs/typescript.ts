e7cd2bfde68dd230fd01f92e98621cbeb9c35d4c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-28 12:59:02 +0100

    feat(typescript): add 'ts/promise-function-async' to type aware rule (#511)

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -46,6 +46,7 @@ export async function typescript(
     'ts/no-unsafe-call': 'error',
     'ts/no-unsafe-member-access': 'error',
     'ts/no-unsafe-return': 'error',
+    'ts/promise-function-async': 'error',
     'ts/restrict-plus-operands': 'error',
     'ts/restrict-template-expressions': 'error',
     'ts/return-await': 'error',
