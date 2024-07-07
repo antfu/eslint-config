48bcd2bc427164ac4ea1fc862ab64d9840c233f4
Anthony Fu <github@antfu.me>
2024-07-07 09:56:17 +0800

    feat: add name to ignore config

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -5,8 +5,7 @@ export async function ignores(): Promise<TypedFlatConfigItem[]> {
   return [
     {
       ignores: GLOB_EXCLUDE,
-      // Awaits https://github.com/humanwhocodes/config-array/pull/131
-      // name: 'antfu/ignores',
+      name: 'antfu/ignores',
     },
   ]
 }
