6e6cd053f9684227650d9ce3e4bb1a69270732e7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-19 21:25:53 +0100

    fix: delete `svelte/valid-compile` rule from eslint-plugin-svelte (#552)

--- src/configs/svelte.ts
+++ src/configs/svelte.ts
@@ -78,7 +78,6 @@ export async function svelte(
         'svelte/no-useless-mustaches': 'error',
         'svelte/require-store-callbacks-use-set-param': 'error',
         'svelte/system': 'error',
-        'svelte/valid-compile': 'error',
         'svelte/valid-each-key': 'error',
 
         'unused-imports/no-unused-vars': [
