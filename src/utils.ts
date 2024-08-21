b60bfd371e18e4fb170a83ca4f78384257cd7df4
Anthony Fu <github@antfu.me>
2024-08-21 16:55:48 +0200

    fix: `isInEditorEnv` should be false when in git hooks, close #589

--- src/utils.ts
+++ src/utils.ts
@@ -132,5 +132,23 @@ export async function ensurePackages(packages: (string | undefined)[]): Promise<
 }
 
 export function isInEditorEnv(): boolean {
-  return !!((process.env.VSCODE_PID || process.env.VSCODE_CWD || process.env.JETBRAINS_IDE || process.env.VIM || process.env.NVIM) && !process.env.CI)
+  if (process.env.CI)
+    return false
+  if (isInGitHooksOrLintStaged())
+    return false
+  return !!(false
+    || process.env.VSCODE_PID
+    || process.env.VSCODE_CWD
+    || process.env.JETBRAINS_IDE
+    || process.env.VIM
+    || process.env.NVIM
+  )
+}
+
+export function isInGitHooksOrLintStaged(): boolean {
+  return !!(false
+    || process.env.GIT_PARAMS
+    || process.env.VSCODE_GIT_COMMAND
+    || process.env.npm_lifecycle_script?.startsWith('lint-staged')
+  )
 }
