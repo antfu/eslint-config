aea78d716b13d551cf5bc167d095fc8c482ff782
Nir Tamir <nirtamir2@gmail.com>
2024-09-24 01:03:40 +0300

    sytlistic jsx

--- src/configs/jsx.ts
+++ src/configs/jsx.ts
@@ -1,7 +1,13 @@
 import { GLOB_JSX, GLOB_TSX } from "../globs";
 import type { TypedFlatConfigItem } from "../types";
+import { interopDefault } from "../utils";
 
 export async function jsx(): Promise<Array<TypedFlatConfigItem>> {
+  const [pluginJsStylistic, pluginJsxStylistic] = await Promise.all([
+    interopDefault(import("@stylistic/eslint-plugin-js")),
+    interopDefault(import("@stylistic/eslint-plugin-jsx")),
+  ] as const);
+
   return [
     {
       files: [GLOB_JSX, GLOB_TSX],
@@ -13,6 +19,18 @@ export async function jsx(): Promise<Array<TypedFlatConfigItem>> {
         },
       },
       name: "antfu/jsx/setup",
+    },
+    {
+      files: [GLOB_JSX, GLOB_TSX],
+      plugins: {
+        "@stylistic/js": pluginJsStylistic,
+        "@stylistic/jsx": pluginJsxStylistic,
+      },
+      name: "nirtamir/jsx/stylistic",
+      rules: {
+        "@stylistic/js/jsx-quotes": "warn",
+        "@stylistic/jsx/jsx-self-closing-comp": "warn",
+      },
     },
   ];
 }
