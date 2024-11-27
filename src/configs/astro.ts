ffe428d8eb29b04e69614ee9f42d9b94c6e11bd8
Kid <44045911+kidonng@users.noreply.github.com>
2024-11-27 09:54:55 +0000

    chore: lint

--- src/configs/astro.ts
+++ src/configs/astro.ts
@@ -43,6 +43,10 @@ export async function astro(
       name: 'antfu/astro/rules',
       processor: 'astro/client-side-ts',
       rules: {
+        // Astro uses top level await for e.g. data fetching
+        // https://docs.astro.build/en/guides/data-fetching/#fetch-in-astro
+        'antfu/no-top-level-await': 'off',
+
         // use recommended rules
         'astro/missing-client-only-directive-value': 'error',
         'astro/no-conflict-set-directives': 'error',
@@ -54,10 +58,6 @@ export async function astro(
         'astro/no-unused-define-vars-in-style': 'error',
         'astro/semi': 'off',
         'astro/valid-compile': 'error',
-
-        // Astro uses top level await for e.g. data fetching
-        // https://docs.astro.build/en/guides/data-fetching/#fetch-in-astro
-        'antfu/no-top-level-await': 'off',
 
         ...stylistic
           ? {
