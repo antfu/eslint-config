69071f1c95e2eeb499c30d3dcb15d9083c2f6efe
Anthony Fu <github@antfu.me>
2025-03-08 02:46:18 +0800

    feat: add more pnpm-package-yaml keys sorting

--- src/configs/yaml.ts
+++ src/configs/yaml.ts
@@ -78,8 +78,23 @@ export async function yaml(
           {
             order: [
               'packages',
+              'overrides',
+              'patchedDependencies',
+              'hoistPattern',
               'catalog',
               'catalogs',
+
+              'allowedDeprecatedVersions',
+              'allowNonAppliedPatches',
+              'configDependencies',
+              'ignoredBuiltDependencies',
+              'ignoredOptionalDependencies',
+              'neverBuiltDependencies',
+              'onlyBuiltDependencies',
+              'onlyBuiltDependenciesFile',
+              'packageExtensions',
+              'peerDependencyRules',
+              'supportedArchitectures',
             ],
             pathPattern: '^$',
           },
