2ae8f1ccd6a8b13b6b6cf9e3dc272e0c479dd49a
Anthony Fu <github@antfu.me>
2025-03-07 18:00:48 +0800

    feat(yaml): sort keys for pnpm workspace

--- src/configs/yaml.ts
+++ src/configs/yaml.ts
@@ -69,5 +69,22 @@ export async function yaml(
         ...overrides,
       },
     },
+    {
+      files: ['pnpm-workspace.yaml'],
+      name: 'antfu/yaml/pnpm-workspace',
+      rules: {
+        'yaml/sort-keys': [
+          'error',
+          {
+            order: [
+              'packages',
+              'catalog',
+              'catalogs',
+            ],
+            pathPattern: '^$',
+          },
+        ],
+      },
+    },
   ]
 }
