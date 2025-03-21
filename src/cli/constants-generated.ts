1866b64919447786027ef8780bcf8fb74966352d
Anthony Fu <github@antfu.me>
2025-03-21 11:27:50 +0900

    chore: update lock

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -1,6 +1,6 @@
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^1.32.0",
-  "@unocss/eslint-plugin": "^66.1.0-beta.3",
+  "@eslint-react/eslint-plugin": "^1.37.0",
+  "@unocss/eslint-plugin": "^66.0.0",
   "astro-eslint-parser": "^1.2.1",
   "eslint": "^9.22.0",
   "eslint-plugin-astro": "^1.3.1",
@@ -8,8 +8,8 @@ export const versionsMap = {
   "eslint-plugin-react-hooks": "^5.2.0",
   "eslint-plugin-react-refresh": "^0.4.19",
   "eslint-plugin-solid": "^0.14.5",
-  "eslint-plugin-svelte": "^3.1.0",
+  "eslint-plugin-svelte": "^3.3.3",
   "prettier-plugin-astro": "^0.14.1",
   "prettier-plugin-slidev": "^1.0.5",
-  "svelte-eslint-parser": "^1.0.1"
+  "svelte-eslint-parser": "^1.1.0"
 }
