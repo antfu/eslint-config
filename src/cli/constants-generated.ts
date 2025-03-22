74edad752ab2ec50f8e2ad9c53c6c58fd2e3c9a2
Anthony Fu <github@antfu.me>
2025-03-23 08:36:12 +0900

    chore: update deps

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -1,8 +1,8 @@
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^1.37.0",
+  "@eslint-react/eslint-plugin": "^1.37.3",
   "@unocss/eslint-plugin": "^66.0.0",
-  "astro-eslint-parser": "^1.2.1",
-  "eslint": "^9.22.0",
+  "astro-eslint-parser": "^1.2.2",
+  "eslint": "^9.23.0",
   "eslint-plugin-astro": "^1.3.1",
   "eslint-plugin-format": "^1.0.1",
   "eslint-plugin-react-hooks": "^5.2.0",
