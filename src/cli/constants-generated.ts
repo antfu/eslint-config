cadc25f1e037941c3b52a3d1b8e879e6f13aa3f2
Anthony Fu <github@antfu.me>
2025-06-23 09:56:36 +0900

    chore: update versions

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -8,7 +8,7 @@ export const versionsMap = {
   "eslint-plugin-react-hooks": "^5.2.0",
   "eslint-plugin-react-refresh": "^0.4.20",
   "eslint-plugin-solid": "^0.14.5",
-  "eslint-plugin-svelte": "^3.9.2",
+  "eslint-plugin-svelte": "^3.9.3",
   "prettier-plugin-astro": "^0.14.1",
   "prettier-plugin-slidev": "^1.0.5",
   "svelte-eslint-parser": "^1.2.0"
