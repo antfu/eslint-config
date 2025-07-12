6df4ff1ef71f0880005c3eff3ff5558d46147794
pacexy <pacexy@gmail.com>
2025-07-12 22:49:13 +0800

    feat: support sort jsconfig

--- src/configs/sort.ts
+++ src/configs/sort.ts
@@ -117,7 +117,7 @@ export async function sortPackageJson(): Promise<TypedFlatConfigItem[]> {
 export function sortTsconfig(): TypedFlatConfigItem[] {
   return [
     {
-      files: ['**/tsconfig.json', '**/tsconfig.*.json'],
+      files: ['**/[jt]sconfig.json', '**/[jt]sconfig.*.json'],
       name: 'antfu/sort/tsconfig-json',
       rules: {
         'jsonc/sort-keys': [
