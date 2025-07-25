aa975821f3570747335eaa02845315773514e19c
Vladyslav Shevchenko <105541001+astrochemx@users.noreply.github.com>
2025-07-25 03:11:19 +0300

    fix: update `TypedFlatConfigItem` to provide rules auto-completion (#742)

--- src/configs/nextjs.ts
+++ src/configs/nextjs.ts
@@ -41,6 +41,7 @@ export async function nextjs(
         },
         sourceType: 'module',
       },
+      name: 'antfu/nextjs/rules',
       rules: {
         ...normalizeRules(pluginNextJS.configs.recommended.rules),
         ...normalizeRules(pluginNextJS.configs['core-web-vitals'].rules),
