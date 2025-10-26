aca34dd5739a6f863dddc49ff199aa5b51765705
Anthony Fu <github@antfu.me>
2025-10-26 15:11:14 +0900

    feat: update deps

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -1,16 +1,16 @@
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^2.2.2",
-  "@next/eslint-plugin-next": "^15.5.5",
-  "@unocss/eslint-plugin": "^66.5.3",
+  "@eslint-react/eslint-plugin": "^2.2.3",
+  "@next/eslint-plugin-next": "^16.0.0",
+  "@unocss/eslint-plugin": "^66.5.4",
   "astro-eslint-parser": "^1.2.2",
-  "eslint": "^9.37.0",
+  "eslint": "^9.38.0",
   "eslint-plugin-astro": "^1.3.1",
   "eslint-plugin-format": "^1.0.2",
-  "eslint-plugin-react-hooks": "^7.0.0",
-  "eslint-plugin-react-refresh": "^0.4.23",
+  "eslint-plugin-react-hooks": "^7.0.1",
+  "eslint-plugin-react-refresh": "^0.4.24",
   "eslint-plugin-solid": "^0.14.5",
-  "eslint-plugin-svelte": "^3.12.4",
+  "eslint-plugin-svelte": "^3.12.5",
   "prettier-plugin-astro": "^0.14.1",
   "prettier-plugin-slidev": "^1.0.5",
-  "svelte-eslint-parser": "^1.3.3"
+  "svelte-eslint-parser": "^1.4.0"
 }
