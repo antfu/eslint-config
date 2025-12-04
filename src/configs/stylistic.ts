9afd5054b372df5f3301030393197e8ea46c9fc1
Vida Xie <vida_2020@163.com>
2025-12-04 13:39:14 +0800

    fix(stylistic): expose `experimental` option (#783)

--- src/configs/stylistic.ts
+++ src/configs/stylistic.ts
@@ -3,6 +3,7 @@ import { pluginAntfu } from '../plugins'
 import { interopDefault } from '../utils'
 
 export const StylisticConfigDefaults: StylisticConfig = {
+  experimental: false,
   indent: 2,
   jsx: true,
   quotes: 'single',
@@ -17,6 +18,7 @@ export async function stylistic(
   options: StylisticOptions = {},
 ): Promise<TypedFlatConfigItem[]> {
   const {
+    experimental,
     indent,
     jsx,
     lessOpinionated = false,
@@ -31,6 +33,7 @@ export async function stylistic(
   const pluginStylistic = await interopDefault(import('@stylistic/eslint-plugin'))
 
   const config = pluginStylistic.configs.customize({
+    experimental,
     indent,
     jsx,
     pluginName: 'style',
@@ -48,8 +51,13 @@ export async function stylistic(
       rules: {
         ...config.rules,
 
+        ...experimental
+          ? {}
+          : {
+              'antfu/consistent-list-newline': 'error',
+            },
+
         'antfu/consistent-chaining': 'error',
-        'antfu/consistent-list-newline': 'error',
 
         ...(lessOpinionated
           ? {
