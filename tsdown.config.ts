6e3725a9a3f87e23c9b30638561a64d345bb6477
locdp <loicduong@users.noreply.github.com>
2026-02-24 11:26:55 +0700

    chore: add inlineOnly configuration for specific packages

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -7,6 +7,17 @@ export default defineConfig({
   ],
   shims: true,
   format: ['esm'],
+  inlineOnly: [
+    'find-up-simple',
+    'eslint-visitor-keys',
+    '@eslint-community/eslint-utils',
+    '@typescript-eslint/utils',
+    '@typescript-eslint/types',
+    '@typescript-eslint/visitor-keys',
+    '@typescript-eslint/scope-manager',
+    'eslint-plugin-erasable-syntax-only',
+    'cached-factory',
+  ],
   exports: {
     legacy: true,
   },
